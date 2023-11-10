d4b82871dc62d8ba2823a8cb8e75399db79bb498
blitz-1306 <hyperx27@gmail.com>
2023-11-10 21:15:42 +0500

    Introduce logic to read Souffle standard output (stdout and file only)

--- src/lib/souffle.ts
+++ src/lib/souffle.ts
@@ -5,6 +5,7 @@ import os from "os";
 import path from "path";
 import * as sol from "solc-typed-ast";
 import { datalogFromUnits } from "./translate";
+import { parse } from "csv-parse/sync";
 
 export function souffle(datalog: string): string {
     const tmpDir = os.tmpdir();
@@ -36,4 +37,60 @@ export function analyze(units: sol.SourceUnit[], analysis: string): string {
     );
 
     return souffle(datalog);
+}
+
+function extractOutputRelations(datalog: string): string[] {
+    const rxOutRel = /\.output\s+(\w+)/g;
+    const result: string[] = [];
+
+    let matches: RegExpExecArray | null;
+
+    while ((matches = rxOutRel.exec(datalog))) {
+        result.push(matches[1]);
+    }
+
+    return result;
+}
+
+export function parseCsv(content: string, delimiter = "\t"): string[][] {
+    const config = {
+        skipEmptyLines: true,
+        cast: true,
+        delimiter
+    };
+
+    return parse(content, config);
+}
+
+export function readProducedCsvFiles(datalog: string): Map<string, string[][]> {
+    const outRels = extractOutputRelations(datalog);
+    const relMap = new Map<string, string[][]>();
+
+    for (const rel of outRels) {
+        const fileName = rel + ".csv";
+
+        if (fse.existsSync(fileName)) {
+            const content = fse.readFileSync(fileName, { encoding: "utf-8" });
+            const entries = parseCsv(content);
+
+            relMap.set(rel, entries);
+
+            fse.removeSync(fileName);
+        }
+    }
+
+    return relMap;
+}
+
+export function readProducedOutput(output: string): Map<string, string[][]> {
+    const rxOutRel = /(\w+)\n+={15,15}\n([^==]+)={15,15}/g;
+    const relMap = new Map<string, string[][]>();
+
+    let matches: RegExpExecArray | null;
+
+    while ((matches = rxOutRel.exec(output))) {
+        relMap.set(matches[1], parseCsv(matches[2]));
+    }
+
+    return relMap;
 }
